7b41a28b3bbe8654ad4da51ddfabae120b15581b
sonicxml <sonicxml@gmail.com>
2012-06-26 18:24:43 -0400

    PegasusQ: Fix bug where it immediately onlines CPU1 right after hotplugging it

--- drivers/cpufreq/cpufreq_pegasusq.c
+++ drivers/cpufreq/cpufreq_pegasusq.c
@@ -899,11 +899,11 @@ static void cpu_up_work(struct work_struct *work)
 	else if (min_cpu_lock)
 		nr_up = max(nr_up, min_cpu_lock - online);
 
-	if (online == 1) {
+/*	if (online == 1) {
 		printk(KERN_ERR "CPU_UP 3\n");
 		cpu_up(num_possible_cpus() - 1);
 		nr_up -= 1;
-	}
+	}*/
 
 	for_each_cpu_not(cpu, cpu_online_mask) {
 		if (nr_up-- == 0)
